8eb5f4e1bb6be8771aba6ae07622a7f6bdf28165
Chaithanya S <chaithanya9ig@gmail.com>
2025-01-19 20:51:40 +0530

    final commit

---
+++ MODAL/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const openModal=function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');    
+   };
+
+   const closeModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');    
+   };  
+
+ for(let i=0;i<btnsOpenModal.length;i++)
+  {
+    btnsOpenModal[i].addEventListener('click',openModal);
+  }; 
+   /*
+    btnCloseModal.addEventListener('click',function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+
+    overlay.addEventListener('click',function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+    */
+   //written separately or can be written as below
+
+   btnCloseModal.addEventListener('click',closeModal);
+   overlay.addEventListener('click',closeModal);
+
+ //inorder to display the key that is pressed from keyboard
+ document.addEventListener('keydown',function(e){
+     console.log('A key is pressed');
+     console.log(e.key);
+
+     if(e.key==='Escape'&&!modal.classList.contains('hidden')){
+            closeModal();    //when Escape key is pressed the Modal closes
+        }
+     });  
+
--- project1.js
+++
@@ -1,2 +0,0 @@
-//GUESS MY NUMBER....!
-
