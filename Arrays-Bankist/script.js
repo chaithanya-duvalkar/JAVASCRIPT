0b0574b37e7eb24f7d4845eda019bc301114bf20
Chaithanya S <chaithanya9ig@gmail.com>
2025-04-21 22:44:05 +0530

    updated Login features

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -157,7 +157,6 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
 // Coding Challenge #1
-
 /* 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 
